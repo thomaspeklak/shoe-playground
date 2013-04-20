36cc92d9f978f080eb4026b6a67522241961bf4c
Thomas Peklak <thomas.peklak@gmail.com>
2013-04-20 22:27:08 +0200

    add file templates for branches

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 "use strict";
 
+var shoe = require("shoe");
+var http = require("http");
 
+var ecstatic = require("ecstatic")(__dirname + "/static");
+
+var server = http.createServer(ecstatic);
+server.listen(3000);
+
+var sock = shoe(function (stream) {
+});
+
+
+sock.install(server, "/socket");
