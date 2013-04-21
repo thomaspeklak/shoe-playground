5e107ebd866a386ab7686fed945561a7444ab0cf
Thomas Peklak <thomas.peklak@gmail.com>
2013-04-21 23:35:32 +0200

    simple muxdemux example with two streams

--- client/client.js
+++ client/client.js
@@ -1,4 +1,25 @@
 (function () {
     "use strict";
     var shoe = require("shoe");
+    var MuxDemux = require("mux-demux");
+
+    var messages = document.getElementById("messages");
+
+    var stream = shoe("/socket");
+    var mdm = MuxDemux(function (stream) {
+        console.dir(stream);
+    });
+
+    mdm.pipe(stream).pipe(mdm);
+
+    var first = mdm.createStream("first");
+    first.on("data", function (data) {
+        messages.textContent += "FIRST: " + data + "\n";
+    });
+
+    var second = mdm.createStream("second");
+    second.on("data", function (data) {
+        messages.textContent += "SECOND: " + data + "\n";
+    });
+
 }());
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var shoe = require("shoe");
 var http = require("http");
+var MuxDemux = require('mux-demux');
 
 var ecstatic = require("ecstatic")(__dirname + "/static");
 
@@ -9,6 +10,24 @@ var server = http.createServer(ecstatic);
 server.listen(3000);
 
 var sock = shoe(function (stream) {
+    console.dir("CONNECTION");
+    var mdm = MuxDemux(function (stream) {
+        if (stream.meta == "first") {
+            setInterval(function () {
+                stream.write("first stream");
+            }, 1000);
+
+        }
+
+        if (stream.meta == "second") {
+            setInterval( function () {
+prestream.write("second");
+            }, 750);
+        }
+    });
+
+    mdm.pipe(stream).pipe(mdm);
+
 });
 
 
