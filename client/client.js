315cd4acf41cfb2c86ef9d6385a3ec6180163ca7
Thomas Peklak <thomas.peklak@gmail.com>
2013-05-03 15:39:38 +0200

    add basic implementation of auth stream

--- client/client.js
+++ client/client.js
@@ -1,4 +1,10 @@
 (function () {
     "use strict";
     var shoe = require("shoe");
+
+    var stream = Auth("user", "secret", sock("/socket");
+
+    stream.on("data", function () {
+        console.dir(arguments);
+    });
 }());
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var shoe = require("shoe");
 var http = require("http");
+var Auth = require("auth-stream");
 
 var ecstatic = require("ecstatic")(__dirname + "/static");
 
@@ -9,7 +10,19 @@ var server = http.createServer(ecstatic);
 server.listen(3000);
 
 var sock = shoe(function (stream) {
-});
+    var auth = Auth(stream, function (user, pass) {
+        if (user === "user" && pass === "secret") {
+            return true;
+        }
 
+        return "ACCESS DENIED";
+    });
+
+    setInterval(function () {
+        stream.write("beep");
+    }, 1000);
+
+    stream.pipe(auth).pipe(stream);
+});
 
 sock.install(server, "/socket");
